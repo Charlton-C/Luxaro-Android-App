85a28702ba07e83bade894dc5dc9624241adbf7b
Charlton <mileswill32@protonmail.com>
2024-04-25 12:46:35 +0300

    Add firebase

--- app/src/main/java/com/example/luxaro/LogIn.kt
+++ app/src/main/java/com/example/luxaro/LogIn.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.luxaro.databinding.ActivityLogInBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LogIn : AppCompatActivity() {
     private lateinit var binding: ActivityLogInBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_log_in)
         binding = ActivityLogInBinding.inflate(layoutInflater)
+        auth = Firebase.auth
     }
 }
--- app/src/main/java/com/example/luxaro/SignUp.kt
+++ app/src/main/java/com/example/luxaro/SignUp.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.luxaro.databinding.ActivitySignUpBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 
 class SignUp : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
+        auth = Firebase.auth
     }
 }
