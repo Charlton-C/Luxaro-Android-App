34afc9713f4ead0cd89c2d328f4e80730da11a54
Charlton <mileswill32@protonmail.com>
2024-04-25 12:29:14 +0300

    Add Log In page design

--- app/src/main/java/com/example/luxaro/LogIn.kt
+++ app/src/main/java/com/example/luxaro/LogIn.kt
@@ -3,16 +3,18 @@ package com.example.luxaro
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.luxaro.databinding.ActivityLogInBinding
 import com.example.luxaro.databinding.ActivitySignUpBinding
 
-
-class SignUp : AppCompatActivity() {
-    private lateinit var binding: ActivitySignUpBinding
+class LogIn : AppCompatActivity() {
+    private lateinit var binding: ActivityLogInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
     }
 }
