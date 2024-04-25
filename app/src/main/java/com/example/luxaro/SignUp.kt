f926df29b256990fcdcaba93f39edf230e865f9f
Charlton <mileswill32@protonmail.com>
2024-04-25 16:43:48 +0300

    Add Sign Up functionality

--- app/src/main/java/com/example/luxaro/SignUp.kt
+++ app/src/main/java/com/example/luxaro/SignUp.kt
@@ -1,11 +1,17 @@
 package com.example.luxaro
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.luxaro.databinding.ActivitySignUpBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.auth.auth
 
 
@@ -16,8 +22,73 @@ class SignUp : AppCompatActivity() {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         auth = Firebase.auth
+
+        binding.signUpButton.setOnClickListener {
+            val name = binding.editTextTextNameInput.text.toString()
+            val email = binding.editTextTextEmailAddressInput.text.toString()
+            val password = binding.editTextTextPasswordInput.text.toString()
+            if (checkAllFields()) {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        it.result.user?.updateProfile(
+                            UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .build()
+                        )
+                        startActivity(Intent(this@SignUp, MainActivity::class.java))
+                        finish()
+                    } else {
+                        if (it.exception is FirebaseAuthUserCollisionException) {
+                            Toast.makeText(this@SignUp, "A user with this email already exists", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(this@SignUp, "Sign Up Failed", Toast.LENGTH_LONG).show()
+                            Log.e("error", it.exception.toString())
+                        }
+                    }
+                }
+            }
+
+        }
+
+        binding.logInButton.setOnClickListener {
+            startActivity(Intent(this@SignUp, LogIn::class.java))
+            finish()
+        }
+    }
+
+    private fun checkAllFields(): Boolean {
+        val email = binding.editTextTextEmailAddressInput.text.toString()
+        if (binding.editTextTextNameInput.text.toString() == ""){
+            binding.editTextTextNameInput.error = "Please enter your name"
+            return false
+        }
+        if (email == ""){
+            binding.editTextTextEmailAddressLayout.error = "Please enter your email"
+            return false
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.editTextTextEmailAddressLayout.error = "Please enter your email using the correct format"
+            return false
+        }
+        if (binding.editTextTextPasswordInput.text.toString() == ""){
+            binding.editTextTextPasswordLayout.error = "Please enter a password"
+            binding.editTextTextPasswordLayout.errorIconDrawable = null
+            return false
+        }
+        if(binding.editTextTextPasswordInput.length() <= 8){
+            binding.editTextTextPasswordLayout.error = "Password is too short"
+            binding.editTextTextPasswordLayout.errorIconDrawable = null
+            return false
+        }
+        if(binding.editTextTextConfirmPasswordInput.text.toString() != binding.editTextTextPasswordInput.text.toString()){
+            binding.editTextTextConfirmPasswordLayout.error = "Passwords don't match"
+            binding.editTextTextConfirmPasswordLayout.errorIconDrawable = null
+            return false
+        }
+        return true
     }
 }
