a4c9f7560799ca3ba0d0219017e012a9ee1cf332
Charlton <mileswill32@protonmail.com>
2024-04-25 12:32:06 +0300

    Optimize files

--- app/src/main/java/com/example/luxaro/LogIn.kt
+++ app/src/main/java/com/example/luxaro/LogIn.kt
@@ -3,10 +3,7 @@ package com.example.luxaro
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.luxaro.databinding.ActivityLogInBinding
-import com.example.luxaro.databinding.ActivitySignUpBinding
 
 class LogIn : AppCompatActivity() {
     private lateinit var binding: ActivityLogInBinding
