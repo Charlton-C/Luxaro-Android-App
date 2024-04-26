7da5025cc7c75befffebcb019b596d8499ce438d
Charlton <mileswill32@protonmail.com>
2024-04-26 22:57:56 +0300

    Change bottom navigation bar icons to match current fragment being viewed

--- app/src/main/java/com/example/luxaro/MainActivity.kt
+++ app/src/main/java/com/example/luxaro/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -46,23 +45,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.ateneo_blue)))
-
-        navView.setOnItemSelectedListener { item ->
-            navView.menu.getItem(0).setIcon(R.drawable.home_icon_outline_24)
-            navView.menu.getItem(1).setIcon(R.drawable.baseline_favorite_border_24)
-            navView.menu.getItem(2).setIcon(R.drawable.baseline_person_outline_24)
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    item.icon = AppCompatResources.getDrawable(this@MainActivity, R.drawable.home_icon_filled_24)
-                }
-                R.id.navigation_favorites -> {
-                    item.icon = AppCompatResources.getDrawable(this@MainActivity, R.drawable.baseline_favorite_24)
-                }
-                R.id.navigation_profile -> {
-                    item.icon = AppCompatResources.getDrawable(this@MainActivity, R.drawable.baseline_person_24)
-                }
-            }
-            false
-        }
     }
 }
