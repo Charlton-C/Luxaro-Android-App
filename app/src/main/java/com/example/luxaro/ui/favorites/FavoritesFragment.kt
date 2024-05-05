77ffad51b6440cb0a75ae4feec4325fcf9c12b08
Charlton <mileswill32@protonmail.com>
2024-05-05 08:38:56 +0300

    Display no properties available when there are no properties available in a users favorites page

--- app/src/main/java/com/example/luxaro/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/luxaro/ui/favorites/FavoritesFragment.kt
@@ -4,9 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import com.example.luxaro.R
 import com.example.luxaro.propertiesLikedByUser
 import com.example.luxaro.ui.home.DisplayProperties
 
@@ -20,8 +38,42 @@ class FavoritesFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                DisplayProperties(properties = propertiesLikedByUser)
+                if (propertiesLikedByUser.isNotEmpty()) {
+                    DisplayProperties(properties = propertiesLikedByUser)
+                }
+                else{
+                    DisplayNoProperties()
+                }
             }
         }
+    }
+}
+
+
+@Composable
+fun DisplayNoProperties(modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(start = 0.dp, top = 0.dp, end = 0.dp, bottom = 58.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState()))
+    {
+        Text(
+            text = stringResource(id = R.string.no_properties_to_display),
+            modifier = modifier
+                .padding(20.dp, 0.dp),
+            fontSize = 24.sp,
+            color = Color.White,
+        )
+        Spacer(modifier = modifier.height(15.dp))
+        Text(
+            text = stringResource(id = R.string.try_liking_one),
+            modifier = modifier
+                .padding(20.dp, 0.dp),
+            fontSize = 20.sp,
+            color = Color.White,
+        )
     }
 }
