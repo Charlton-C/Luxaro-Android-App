01ad8454495c09ee558e3ca9f94e839910d395de
Charlton <mileswill32@protonmail.com>
2024-05-05 08:37:54 +0300

    Display favorite properties

--- app/src/main/java/com/example/luxaro/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/luxaro/ui/favorites/FavoritesFragment.kt
@@ -4,39 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.luxaro.databinding.FragmentFavoritesBinding
+import com.example.luxaro.propertiesLikedByUser
+import com.example.luxaro.ui.home.DisplayProperties
 
 class FavoritesFragment : Fragment() {
-
-    private var _binding: FragmentFavoritesBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(FavoritesViewModel::class.java)
-
-        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textFavorites
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                DisplayProperties(properties = propertiesLikedByUser)
+            }
         }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
