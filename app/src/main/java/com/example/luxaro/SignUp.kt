364662aaa1c1f316c5b2405fc2640ec55d8d212b
Charlton <mileswill32@protonmail.com>
2024-06-09 19:09:48 +0300

    Reformat SignUp and activity_sign_up

--- app/src/main/java/com/example/luxaro/SignUp.kt
+++ app/src/main/java/com/example/luxaro/SignUp.kt
@@ -46,13 +46,20 @@ class SignUp : AppCompatActivity() {
                     showSigningUpAnimation(false)
                     startActivity(Intent(this@SignUp, MainActivity::class.java))
                     finish()
-                }.addOnFailureListener{
+                }.addOnFailureListener {
                     if (it is FirebaseAuthUserCollisionException) {
-                        binding.editTextTextEmailAddressLayout.error = resources.getString(R.string.a_user_with_this_email_already_exists)
-                    }
-                    else{
-                        Toast.makeText(this@SignUp, resources.getString(R.string.sign_up_failed_LineBreak_please_try_again_later), Toast.LENGTH_SHORT).show()
-                        Log.e(resources.getString(R.string.firebase_auth_error_colon_sign_up), it.toString())
+                        binding.editTextTextEmailAddressLayout.error =
+                            resources.getString(R.string.a_user_with_this_email_already_exists)
+                    } else {
+                        Toast.makeText(
+                            this@SignUp,
+                            resources.getString(R.string.sign_up_failed_LineBreak_please_try_again_later),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        Log.e(
+                            resources.getString(R.string.firebase_auth_error_colon_sign_up),
+                            it.toString()
+                        )
                     }
                     showSigningUpAnimation(false)
                 }
@@ -68,46 +75,52 @@ class SignUp : AppCompatActivity() {
 
     private fun checkAllFields(): Boolean {
         val email = binding.editTextTextEmailAddressInput.text.toString()
-        if (binding.editTextTextNameInput.text.toString() == ""){
-            binding.editTextTextNameLayout.error = resources.getString(R.string.please_enter_your_name)
+        if (binding.editTextTextNameInput.text.toString() == "") {
+            binding.editTextTextNameLayout.error =
+                resources.getString(R.string.please_enter_your_name)
             return false
         }
-        if (email == ""){
-            binding.editTextTextEmailAddressLayout.error = resources.getString(R.string.please_enter_your_email)
+        if (email == "") {
+            binding.editTextTextEmailAddressLayout.error =
+                resources.getString(R.string.please_enter_your_email)
             return false
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            binding.editTextTextEmailAddressLayout.error = resources.getString(R.string.please_enter_your_email_using_the_correct_format)
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.editTextTextEmailAddressLayout.error =
+                resources.getString(R.string.please_enter_your_email_using_the_correct_format)
             return false
         }
-        if (binding.editTextTextPasswordInput.text.toString() == ""){
-            binding.editTextTextPasswordLayout.error = resources.getString(R.string.please_enter_a_password)
+        if (binding.editTextTextPasswordInput.text.toString() == "") {
+            binding.editTextTextPasswordLayout.error =
+                resources.getString(R.string.please_enter_a_password)
             binding.editTextTextPasswordLayout.errorIconDrawable = null
             return false
         }
-        if(binding.editTextTextPasswordInput.length() <= 8){
-            binding.editTextTextPasswordLayout.error = resources.getString(R.string.password_is_too_short)
+        if (binding.editTextTextPasswordInput.length() <= 8) {
+            binding.editTextTextPasswordLayout.error =
+                resources.getString(R.string.password_is_too_short)
             binding.editTextTextPasswordLayout.errorIconDrawable = null
             return false
         }
-        if(binding.editTextTextConfirmPasswordInput.text.toString() != binding.editTextTextPasswordInput.text.toString()){
-            binding.editTextTextConfirmPasswordLayout.error = resources.getString(R.string.passwords_dont_match)
+        if (binding.editTextTextConfirmPasswordInput.text.toString() != binding.editTextTextPasswordInput.text.toString()) {
+            binding.editTextTextConfirmPasswordLayout.error =
+                resources.getString(R.string.passwords_dont_match)
             binding.editTextTextConfirmPasswordLayout.errorIconDrawable = null
             return false
         }
         return true
     }
 
-    private fun showSigningUpAnimation(show: Boolean){
+    private fun showSigningUpAnimation(show: Boolean) {
         // Start signing up animation
-        if (show){
+        if (show) {
             binding.signUpButton.visibility = View.GONE
             binding.signUpProgressBar.visibility = View.VISIBLE
             binding.logInButton.isEnabled = false
             binding.logInButton.isClickable = false
         }
         // End signing up animation
-        else{
+        else {
             binding.signUpButton.visibility = View.VISIBLE
             binding.signUpProgressBar.visibility = View.GONE
             binding.logInButton.isEnabled = true
